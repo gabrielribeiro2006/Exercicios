76da433565f9b89bb625f759d38e3d1dfac0fb5f
gabrielribeiro2006 <160604464+gabrielribeiro2006@users.noreply.github.com>
2024-02-26 14:36:45 -0300

    Add files via upload

---
+++ aula8/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamenetos
+fun main(args: Array<String>) {
+    var a: Int = 33.dec()
+    var b : String = a.toString()
+    println("Int: $a")
+    println("Primeiro char da string b é: ${b.first()}")
+}
